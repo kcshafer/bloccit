3f05d7b5fcf865156bae6176de9c7ab1569e1507
kcshafer <kcplusplus@live.com>
2016-03-07 16:32:39 -0500

    Completed checkpoint 32

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -2,18 +2,33 @@ class PostsController < ApplicationController
   def index
     @posts = []
     Post.all.each_with_index do |p,i|
-        puts p
         if i % 5 == 0 then
             p.title = 'SPAM'
         end
-        @posts << p
+            @posts << p
+        end
     end
-  end
 
-  def show
-  end
+    def show
+        @post = Post.find(params[:id])  
+    end
 
   def new
+    @post = Post.new
+  end
+
+  def create
+    @post = Post.new
+    @post.title = params[:post][:title]
+    @post.body = params[:post][:body]
+
+    if @post.save
+        flash[:notice] = "Post was saved."
+        redirect_to @post
+    else
+        @flash.now[:alert] = "There was an error, please try again"
+        render :new
+    end
   end
 
   def edit
