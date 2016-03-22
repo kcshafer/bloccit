3abf859e29e0120f9d45f075ff0f62cf9d48e1b6
kcshafer <kcplusplus@live.com>
2016-03-21 20:32:22 -0400

    completed assignment 36

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,6 @@
 class User < ActiveRecord::Base
     before_save { self.email = email.downcase }
+    before_save :make_title_case
 
     validates :name, length: { minimum: 1, maximum: 100 }, presence: true
     validates :password, presence: true, length: { minimum: 6 }, if: "password_digest.nil?"
@@ -8,4 +9,14 @@ class User < ActiveRecord::Base
     validates :email, presence: true, uniqueness: { case_sensitive: false }, length: { minimum: 3, maximum: 254 }
 
     has_secure_password
+
+    def make_title_case
+        new_name = []
+        if self.name != nil
+            self.name.split.map do |n|
+                new_name << n.capitalize
+            end
+            self.name = new_name.join(' ')
+        end
+    end
 end
